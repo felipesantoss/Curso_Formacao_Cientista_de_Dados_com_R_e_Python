63dbe4cf0300728241b3ba073bc1d362ac5208ab
Felipe dos Santos Silva <felipe.dsantossilva@outlook.com>
2019-04-20 17:43:47 -0300

    Estatistica I

---
+++ src/Python/amostragem_estratificada.py
@@ -0,0 +1,32 @@
+#coding: utf-8
+
+#import Pandas 
+#pip install pandas
+import pandas as pd
+#import Sklearn 
+#pip install -U scikit-learn scipy matplotlib
+from sklearn.model_selection import train_test_split
+#Import Numpy
+import numpy as np
+
+#Lendo o iris.csv
+base = pd.read_csv('../Dados/iris.csv')
+
+#Imprimindo a quantidade de linhas e a contidade de colunas no arquivo
+print('Tolal de linhas: {linhas} Total de colunas: {colunas}'.format(linhas = str(base.shape[0]), colunas =  str(base.shape[1])))
+
+print('Contador de elementos agrupados por Class:\n{totalClass}'.format(totalClass = base['class'].value_counts()))
+
+x, _, y, _ = train_test_split(base.iloc[:,0:4], base.iloc[:, 4], test_size = 0.5, stratify = base.iloc[:, 4])
+
+print('Total das amostras estatificadas:\n{totalEstatificada}'.format(totalEstatificada = y.value_counts()))
+
+
+#Lendo o iris.csv
+base_infert = pd.read_csv('../Dados/infert.csv')
+
+print('Contador de elementos agrupados por Education :\n{totalEducation}'.format(totalEducation = base_infert['education'].value_counts()))
+
+x1, _, y1, _ = train_test_split(base_infert.iloc[:, 2:9], base_infert.iloc[:, 1], test_size = 0.6, stratify = base_infert.iloc[:, 1])
+
+print('Total das amostras estatificadas:\n{totalEstatificada}'.format(totalEstatificada = y1.value_counts()))
---
+++ src/Python/amostragem_simplay.py
@@ -0,0 +1,22 @@
+#coding: utf-8
+
+#import Pandas 
+#pip install pandas
+import pandas as pd
+#Import Numpy
+import numpy as np
+
+#Lendo o iris.csv
+base = pd.read_csv('../Dados/iris.csv')
+
+#Imprimindo a quantidade de linhas e a contidade de colunas no arquivo
+print('Tolal de linhas: {linhas} \nTotal de colunas: {colunas}'.format(linhas = str(base.shape[0]), colunas =  str(base.shape[1])))
+
+#Gerando os numeros aleatorios para gerar a amostra
+amostra = np.random.choice(a = [0, 1], size = 150, replace = True, p = [0.5, 0.5])
+
+print('Contidade total da variavel amostra: {total}'.format(total = len(amostra)))
+
+print('Contidade total de 1 na variavel amostra: {total}'.format(total = len(amostra[amostra == 1])))
+
+print('Contidade total de 0 na variavel amostra: {total}'.format(total = len(amostra[amostra == 0])))
